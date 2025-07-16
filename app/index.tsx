e110f6c389f50529a5cc6f5b1d0770216b423142
Vicente <vicenteferrer_01@hotmail.com>
2025-07-16 19:47:43 -0400

    Integrating React Native Maps

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Region } from 'react-native-maps';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const mainRegion: Region = {
+	latitude: -42.65,
+	longitude: -73.85,
+	latitudeDelta: 0.5,
+	longitudeDelta: 1.25
+};
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<MapView style={styles.map} region={mainRegion} initialRegion={mainRegion} />
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	map: {
+		width: '100%',
+		height: '100%'
+	}
+});
